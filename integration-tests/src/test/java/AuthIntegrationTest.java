7b2f27bb59d8613479a6adfba30c8ea7f8c49cf4
smruti-banty <srnayak165@gmail.com>
2025-08-13 11:40:58 +0530

    integration test module init

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,41 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+  @BeforeAll
+  public static void setUp() {
+    RestAssured.baseURI = "http://localhost:4004";
+  }
+
+  @Test
+  public void shouldReturnOKWithValidToken() {
+    // 1. Arrange
+    String loginPayload = """
+        {
+          "email": "testuser@test.com",
+          "password": "password123"
+        }
+        """;
+
+    // 2. Act
+    Response response = given()
+        .contentType("application/json")
+        .body(loginPayload)
+        .when()
+        .post("/auth/login")
+    // 3. Assert
+        .then()
+        .statusCode(200)
+        .body("token", notNullValue())
+        .extract()
+        .response();
+
+
+    System.out.println("Generated Token: " + response.jsonPath().getString("token"));
+  }
+}
